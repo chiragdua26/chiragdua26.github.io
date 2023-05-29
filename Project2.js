58073c33a9e45f5abd956e345994cbf5ddc4b68e
CHIRAG DUA <68508893+chiragdua26@users.noreply.github.com>
2023-05-29 09:38:52 +0530

    Create Project2.js

---
+++ Project2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project2 = () => {
+  return (
+    <div>
+      <h1>Project 2</h1>
+      {/* Add your second project content */}
+    </div>
+  );
+};
+
+export default Project2;
