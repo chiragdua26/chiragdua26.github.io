d0853482f6cc12b6bfe4a1d7cc539e64eb668d62
CHIRAG DUA <68508893+chiragdua26@users.noreply.github.com>
2023-05-29 09:39:07 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      {/* Add your contact page content */}
+    </div>
+  );
+};
+
+export default Contact;
