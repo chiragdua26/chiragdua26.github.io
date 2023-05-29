d5f9b5cbc9ba6a2280cfc914613f4e9482b8ae72
CHIRAG DUA <68508893+chiragdua26@users.noreply.github.com>
2023-05-29 09:38:37 +0530

    Create Project1.js

---
+++ Project1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Project1 = () => {
+  return (
+    <div>
+      <h1>Project 1</h1>
+      {/* Add your first project content */}
+    </div>
+  );
+};
+
+export default Project1;
